858690c4941a21f22c37b705f87e77dfb9b8f677
tal <anlv.tan@tendcloud.com>
2014-03-31 15:14:40 +0800

    fix reader error.

--- examples/read.js
+++ examples/read.js
@@ -3,26 +3,36 @@ var co = require('co'),
     Reader = require('../').Reader,
     LineReader = require('../').LineReader;
 
+function sleep(mill) {
+    return function (cb) { setTimeout(cb, mill); };
+}
+
 co(function *() {
-    var input = fs.createReadStream('read.js'),
+    var input = fs.createReadStream('test.log'),
         reader = new Reader(input),
         txt;
 
     while (txt = yield reader.read('utf8')) {
         console.log(txt);
+        //yield sleep(100);
     }
+
+    console.log('done');
 })(function (err) {
     if (err) console.log(err);
 });
 
 co(function *() {
-    var input = fs.createReadStream('read.js'),
+    var input = fs.createReadStream('test.log'),
         reader = new LineReader(input),
         txt;
 
     while (typeof (txt = yield reader.read('utf8')) === 'string') {
         console.log('line: ' + txt);
+        yield sleep(1);
     }
+
+    console.log('done');
 })(function (err) {
     if (err) console.log(err);
 });
