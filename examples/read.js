0bd984472e2c473e9c698c0a4d8106e95a3e4ea2
tal <anlv.tan@tendcloud.com>
2014-03-31 11:55:49 +0800

    LineReader

--- examples/read.js
+++ examples/read.js
@@ -1,6 +1,7 @@
 var co = require('co'),
     fs = require('fs'),
-    Reader = require('../').Reader;
+    Reader = require('../').Reader,
+    LineReader = require('../').LineReader;
 
 co(function *() {
     var input = fs.createReadStream('read.js'),
@@ -9,6 +10,18 @@ co(function *() {
 
     while (txt = yield reader.read('utf8')) {
         console.log(txt);
+    }
+})(function (err) {
+    if (err) console.log(err);
+});
+
+co(function *() {
+    var input = fs.createReadStream('read.js'),
+        reader = new LineReader(input),
+        txt;
+
+    while (typeof (txt = yield reader.read('utf8')) === 'string') {
+        console.log('line: ' + txt);
     }
 })(function (err) {
     if (err) console.log(err);
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 exports.Reader = require('./lib/reader.js').Reader;
+exports.LineReader = require('./lib/reader').LineReader;
