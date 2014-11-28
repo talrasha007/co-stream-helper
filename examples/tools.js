2cc32964e9d63f713e29cad753ab259e6b5375f3
Tal <talrasha007@gmail.com>
2014-11-29 01:52:37 +0800

    stream modes.

--- examples/tools.js
+++ examples/tools.js
@@ -5,15 +5,15 @@ var _ = require('codash'),
 
 var total = 0;
 fs.createReadStream('test.log')
-    .pipe(cs.map(function *(data) { total += data.length; yield _.sleep(1000); return data.slice(0, 1); }, { parallel: 3 }))
+    .pipe(cs.string.map(function *(data) { total += data.length; yield _.sleep(1000); return data.slice(0, 1); }, { parallel: 3 }))
     .pipe(process.stdout);
 
 var Emiter = require('events').EventEmitter,
     em = new Emiter();
 
-var evs = cs.fromEmmiter(em, { objectMode: true });
+var evs = cs.object.fromEmmiter(em, { objectMode: true });
 
-evs.pipe(cs.map(function *(data) { return JSON.stringify(data); }, { objectMode: true })).pipe(process.stdout);
+evs.pipe(cs.object.map(function *(data) { return JSON.stringify(data); })).pipe(process.stdout);
 
 em.emit('data', { a: 1 });
 
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+var _ = require('codash');
+
 exports.Reader = require('./lib/reader.js').Reader;
 exports.LineReader = require('./lib/reader.js').LineReader;
 exports.Writer = require('./lib/writer.js').Writer;
 
 var MapStream = require('./lib/map-stream.js');
-exports.map = function (mapper, opt) { return new MapStream(mapper, opt); };
-
 var EventStream = require('./lib/event-stream.js');
-exports.fromEmmiter = function (em, opt) { return new EventStream(em, opt); };
+
+exports.object = {
+    map: function (mapper, opt) { return new MapStream(mapper, _.defaults(opt || {}, { objectMode: true })); },
+    fromEmmiter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { objectMode: true })); }
+};
+
+exports.string = {
+    map: function (mapper, opt) { return new MapStream(mapper, _.defaults(opt || {}, { encoding: 'utf8' })); },
+    fromEmmiter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { encoding: 'utf8' })); }
+};
