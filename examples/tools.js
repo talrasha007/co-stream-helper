ec7430ece7c10aad3789fb981a099125527c42c6
Tal <talrasha007@gmail.com>
2014-11-29 13:25:51 +0800

    fix emitter spell. -.-

--- examples/tools.js
+++ examples/tools.js
@@ -9,10 +9,10 @@ fs.createReadStream('test.log')
     .pipe(cs.string.map(function *(data) { total += data.length; yield _.sleep(1000); return data.slice(0, 1); }, { parallel: 3 }))
     .pipe(process.stdout);
 
-var Emiter = require('events').EventEmitter,
-    em = new Emiter();
+var Emitter = require('events').EventEmitter,
+    em = new Emitter();
 
-var evs = cs.object.fromEmmiter(em, { objectMode: true });
+var evs = cs.object.fromEmitter(em, { objectMode: true });
 
 evs.pipe(cs.object.map(function *(data) { return _.extend({ xx: data.a }, data); }))
    .pipe(cs.object.each(function *(data) { yield _.sleep(1000); console.log(data); }, { parallel: 2 }));
--- index.js
+++ index.js
@@ -27,17 +27,17 @@ var EachStream = require('./lib/each-stream.js'),
 exports.object = {
     each: function (iterator, opt) { return new EachStream(iterator, _.defaults(opt || {}, { objectMode: true })); },
     map: function (mapper, opt) { return new MapStream(mapper, _.defaults(opt || {}, { objectMode: true })); },
-    fromEmmiter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { objectMode: true })); }
+    fromEmitter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { objectMode: true })); }
 };
 
 exports.string = {
     each: function (iterator, opt) { return new EachStream(iterator, _.defaults(opt || {}, { encoding: 'utf8' })); },
     map: function (mapper, opt) { return new MapStream(mapper, _.defaults(opt || {}, { encoding: 'utf8' })); },
-    fromEmmiter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { encoding: 'utf8' })); }
+    fromEmitter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { encoding: 'utf8' })); }
 };
 
 _.extend(exports, {
     each: function (iterator, opt) { return new EachStream(iterator, opt); },
     map: function (mapper, opt) { return new MapStream(mapper, opt); },
-    fromEmmiter: function (em, opt) { return new EventStream(em, opt); }
+    fromEmitter: function (em, opt) { return new EventStream(em, opt); }
 });
