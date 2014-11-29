ccb38f9bcf8aaf37c1de6b08a7ae7bab634651b3
Tal <talrasha007@gmail.com>
2014-11-29 11:13:20 +0800

    add example for split. update engine in package.json

---
+++ examples/split.js
@@ -0,0 +1,12 @@
+var _ = require('codash'),
+    co = require('co'),
+    fs = require('fs'),
+
+    cs = require('../');
+
+fs.createReadStream('test.log', {flags: 'r'})
+    .pipe(cs.split())
+    .pipe(cs.object.each(function *(line) {
+        //do something with the line
+        console.log('line: ', line);
+    }));
