ff007477804122025583ec102dee91200147986a
ttl <talrasha007@gmail.com>
2017-04-26 18:05:26 +0800

    es6 style.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var _ = require('co-lodash'),
-    through = require('through'),
-    duplex = require('duplexer'),
-    pause = require('pause-stream'),
-    split = require('split');
+const _ = require('co-lodash');
+const through = require('through');
+const duplex = require('duplexer');
+const pause = require('pause-stream');
+const split = require('split');
 
 exports.through = through;
 exports.duplex = duplex;
@@ -10,34 +10,34 @@ exports.pause = pause;
 exports.split = split;
 
 exports.wait = function *(stream) {
-    yield function (cb) {
-        stream.on('end', cb);
-        stream.on('finish', cb);
-    };
+  yield cb => {
+    stream.on('end', cb);
+    stream.on('finish', cb);
+  };
 };
 
 exports.Reader = require('./lib/reader.js').Reader;
 exports.LineReader = require('./lib/reader.js').LineReader;
 exports.Writer = require('./lib/writer.js').Writer;
 
-var EachStream = require('./lib/each-stream.js'),
-    MapStream = require('./lib/map-stream.js'),
-    EventStream = require('./lib/event-stream.js');
+const EachStream = require('./lib/each-stream.js');
+const MapStream = require('./lib/map-stream.js');
+const EventStream = require('./lib/event-stream.js');
 
 exports.object = {
-    each: function (iterator, opt) { return new EachStream(iterator, _.defaults(opt || {}, { objectMode: true })); },
-    map: function (mapper, opt) { return new MapStream(mapper, _.defaults(opt || {}, { objectMode: true })); },
-    fromEmitter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { objectMode: true })); }
+  each: (iterator, opt) => new EachStream(iterator, _.defaults(opt || {}, { objectMode: true })),
+  map: (mapper, opt) => new MapStream(mapper, _.defaults(opt || {}, { objectMode: true })),
+  fromEmitter: (em, opt) => new EventStream(em, _.defaults(opt || {}, { objectMode: true }))
 };
 
 exports.string = {
-    each: function (iterator, opt) { return new EachStream(iterator, _.defaults(opt || {}, { encoding: 'utf8' })); },
-    map: function (mapper, opt) { return new MapStream(mapper, _.defaults(opt || {}, { encoding: 'utf8' })); },
-    fromEmitter: function (em, opt) { return new EventStream(em, _.defaults(opt || {}, { encoding: 'utf8' })); }
+  each: (iterator, opt) => new EachStream(iterator, _.defaults(opt || {}, { encoding: 'utf8' })),
+  map: (mapper, opt) => new MapStream(mapper, _.defaults(opt || {}, { encoding: 'utf8' })),
+  fromEmitter: (em, opt) => new EventStream(em, _.defaults(opt || {}, { encoding: 'utf8' }))
 };
 
 _.extend(exports, {
-    each: function (iterator, opt) { return new EachStream(iterator, opt); },
-    map: function (mapper, opt) { return new MapStream(mapper, opt); },
-    fromEmitter: function (em, opt) { return new EventStream(em, opt); }
+  each: (iterator, opt) => new EachStream(iterator, opt),
+  map: (mapper, opt) => new MapStream(mapper, opt),
+  fromEmitter: (em, opt) => new EventStream(em, opt)
 });
