4c642c56998749e791df0db64ea3ef95798bb372
Tal Rasha <talrasha007@gmail.com>
2017-04-29 12:14:34 +0800

    IterableStream constructor support Promise.

--- examples/iterable.js
+++ examples/iterable.js
@@ -1,6 +1,6 @@
 const cs = require('../');
 
-cs.fromIterable([1, 2, 3, 4, 5, 6, 7, 8])
-  .pipe(cs.object.each(it => {
-    console.log(it);
-  }));
+cs.fromIterable([1, 2, 3, 4, 5, 6, 7, 8]).pipe(cs.object.each(console.log));
+
+const promise = new Promise(resolve => setTimeout(() => resolve(['a', 'b', 'c']), 1000));
+cs.fromIterable(promise).pipe(cs.object.each(console.log));
