9e16c348014f1b94a1741ba94dfaa7624abb3113
ttl <talrasha007@gmail.com>
2017-12-01 11:54:20 +0800

    add sync api to improve performance.

--- index.js
+++ index.js
@@ -19,30 +19,41 @@ exports.Reader = require('./lib/reader.js').Reader;
 exports.LineReader = require('./lib/reader.js').LineReader;
 exports.Writer = require('./lib/writer.js').Writer;
 
-const EachStream = require('./lib/each-stream.js');
-const MapStream = require('./lib/map-stream.js');
+const EachStream = {
+  sync: require('./lib/sync/each-stream.js'),
+  async: require('./lib/async/each-stream.js')
+};
+
+const MapStream = {
+  sync: require('./lib/sync/map-stream.js'),
+  async: require('./lib/async/map-stream.js')
+};
+
+const getEachStream = (iter, opt) => (opt.async || _.isGenerator(iter) || _.isAsyncFunction(iter)) ? EachStream.async(iter, opt) : EachStream.sync(iter, opt);
+const getMapStream = (iter, opt) => (opt.async || _.isGenerator(iter) || _.isAsyncFunction(iter)) ? MapStream.async(iter, opt) : MapStream.sync(iter, opt);
+
 const EventStream = require('./lib/event-stream.js');
 const IterableStream = require('./lib/iterable-stream');
 
 exports.object = {
-  each: (iterator, opt) => new EachStream(iterator, _.defaults(opt || {}, { objectMode: true })),
-  map: (mapper, opt) => new MapStream(mapper, _.defaults(opt || {}, { objectMode: true })),
-  flatMap: (mapper, opt) => new MapStream(mapper, _.defaults(opt || {}, { flatten: true, objectMode: true })),
-  filter: (filter, opt) => new MapStream(filter, _.defaults(opt || {}, { objectMode: true, isFilter: true  })),
+  each: (iterator, opt) => getEachStream(iterator, _.defaults(opt || {}, { objectMode: true })),
+  map: (mapper, opt) => getMapStream(mapper, _.defaults(opt || {}, { objectMode: true })),
+  flatMap: (mapper, opt) => getMapStream(mapper, _.defaults(opt || {}, { flatten: true, objectMode: true })),
+  filter: (filter, opt) => getMapStream(filter, _.defaults(opt || {}, { objectMode: true, isFilter: true  })),
   fromEmitter: (em, opt) => new EventStream(em, _.defaults(opt || {}, { objectMode: true }))
 };
 
 exports.string = {
-  each: (iterator, opt) => new EachStream(iterator, _.defaults(opt || {}, { encoding: 'utf8' })),
-  map: (mapper, opt) => new MapStream(mapper, _.defaults(opt || {}, { encoding: 'utf8' })),
-  flatMap: (mapper, opt) => new MapStream(mapper, _.defaults(opt || {}, { flatten: true, encoding: 'utf8' })),
-  filter: (filter, opt) => new MapStream(filter, _.defaults(opt || {}, { encoding: 'utf8', isFilter: true })),
+  each: (iterator, opt) => getEachStream(iterator, _.defaults(opt || {}, { encoding: 'utf8' })),
+  map: (mapper, opt) => getMapStream(mapper, _.defaults(opt || {}, { encoding: 'utf8' })),
+  flatMap: (mapper, opt) => getMapStream(mapper, _.defaults(opt || {}, { flatten: true, encoding: 'utf8' })),
+  filter: (filter, opt) => getMapStream(filter, _.defaults(opt || {}, { encoding: 'utf8', isFilter: true })),
   fromEmitter: (em, opt) => new EventStream(em, _.defaults(opt || {}, { encoding: 'utf8' }))
 };
 
 _.extend(exports, {
-  each: (iterator, opt) => new EachStream(iterator, opt),
-  map: (mapper, opt) => new MapStream(mapper, opt),
+  each: (iterator, opt) => getEachStream(iterator, opt),
+  map: (mapper, opt) => getMapStream(mapper, opt),
   fromIterable: iterable => new IterableStream(iterable),
   fromEmitter: (em, opt) => new EventStream(em, opt)
 });
